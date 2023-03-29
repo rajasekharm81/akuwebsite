847a459d6a1abe5262da97c6bc6d9ffc37cf6da1
rajasekharm81 <102529517+rajasekharm81@users.noreply.github.com>
2023-03-29 18:41:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { Component } from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 import "./App.css";
-import Home from "./landingPage";
+// import Home from "./landingPage";Cons
+import {Cons} from "./landingPage";
 import Vc from "./Abouts";
 import {
   History,
@@ -17,7 +18,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Cons />} />
           <Route exact path="aboutUs/governingBody" element={<Vc />} />
           <Route exact path="aboutUs/history" element={<History />} />
           <Route
